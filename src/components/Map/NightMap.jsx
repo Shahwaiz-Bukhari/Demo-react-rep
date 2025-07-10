52976df9a5e3e8ad87e24c3451573da44ecaf5a1
Shahwaiz-Bukhari <162057979+Shahwaiz-Bukhari@users.noreply.github.com>
2025-07-10 17:13:27 +0500

    mapping

--- src/components/Map/NightMap.jsx
+++ src/components/Map/NightMap.jsx
@@ -1,14 +1,29 @@
 import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import './NightMap.css';
 
 export default function NightMap() {
   useEffect(() => {
     const existingMap = document.getElementById('map');
     if (existingMap && existingMap._leaflet_id) {
-      return; 
+      return;
     }
+
+    const DefaultIcon = L.icon({
+      iconUrl: markerIcon,
+      iconRetinaUrl: markerIcon2x,
+      shadowUrl: markerShadow,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      tooltipAnchor: [16, -28],
+      shadowSize: [41, 41],
+    });
+    L.Marker.prototype.options.icon = DefaultIcon;
 
     const map = L.map('map').setView([31.4622, 74.2796], 13);
 
